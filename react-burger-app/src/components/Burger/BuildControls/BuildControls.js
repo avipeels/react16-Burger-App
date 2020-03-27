b31bf609f4e7645fa73dae7a9bfa5c47c0ced275
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-03-27 15:44:37 +0530

    Build Controls

---
+++ react-burger-app/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
+const buildControls = (props) => {
+    return (
+        <div className={classes.buildControls}>
+            {controls.map(control => (
+                <BuildControl key={control.label} type={control.type} />
+            ))}
+        </div>
+    );
+}
+
+export default buildControls;
