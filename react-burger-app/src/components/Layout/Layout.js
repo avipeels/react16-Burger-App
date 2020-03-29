fcdddb71df0c43f5f1a799d899c5aa1202ff46d7
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-03-30 00:04:21 +0530

    Side Drawer

--- react-burger-app/src/components/Layout/Layout.js
+++ react-burger-app/src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Aux from '../../hoc/Auxilary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 const layout = (props) => (
     <Aux>
         <div>
             <Toolbar/>
+            <SideDrawer/>
         </div>
         <main className={classes.Content}>
             {props.children}
