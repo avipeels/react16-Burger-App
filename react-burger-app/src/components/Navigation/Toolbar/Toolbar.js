31b88f547864393fb2a357e8d1eb26c5c139e075
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-03-29 23:23:09 +0530

    navigation

--- react-burger-app/src/components/Navigation/Toolbar/Toolbar.js
+++ react-burger-app/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 const Toolbar = (props) => {
     return (
         <header className={classes.Toolbar}>
@@ -9,7 +10,7 @@ const Toolbar = (props) => {
             </div>
             <Logo />
             <nav>
-                ...
+                <NavigationItems />
             </nav>
         </header>
     );
