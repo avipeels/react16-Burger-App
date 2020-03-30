2505c65db5e1108bae399b4086029b9b960e86c2
Peelukhana Sai Krishna Avinash <avipeelu@publicisgroupe.net>
2020-03-30 15:11:54 +0530

    Layout to class comp, Drawer toggle mobile menu

--- react-burger-app/src/components/Layout/Layout.js
+++ react-burger-app/src/components/Layout/Layout.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from '../../hoc/Auxilary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <Aux>
-        <div>
-            <Toolbar/>
-            <SideDrawer/>
-        </div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true,
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false,
+        });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {
+                showSideDrawer: !prevState.showSideDrawer,
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <div>
+                    <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                    <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                </div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
